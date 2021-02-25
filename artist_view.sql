37e8bbe37395ccec0b11f8b6417e27273eb8d48f
Magdalena Podolewska <magdalena@podolewska.com>
2021-02-25 13:46:30 +0100

    feat: index task executed

--- artist_view.sql
+++ artist_view.sql
@@ -7,13 +7,14 @@ CREATE OR REPLACE VIEW artist_view AS
     LEFT JOIN artist ON artist.id = song_artist.artist_id;
 
 CREATE OR REPLACE FUNCTION get_artist_songs(id_num int)
-    RETURNS TABLE(name_var CHAR(150), data_var DATE, duration_var DOUBLE PRECISION)
+    RETURNS TABLE(name_var CHAR(150), data_var DATE, duration_var FLOAT)
+    language plpgsql
     AS
     $$
     BEGIN
         RETURN QUERY
         SELECT name, date, duration FROM artist_view WHERE id = id_num;
     END;
-    $$ language plpgsql;
+    $$;
 
 SELECT get_artist_songs(1000);
--- user_email_with_index.sql
+++ user_email_with_index.sql
@@ -1,2 +1,6 @@
 -- 3. Index na tabeli user, dla kolumny email oraz przykładowe zapytanie do bazy,
--- aby sprawdzić jak szybko to zapytanie będzie działać na bazie jeśli indexu nie będzie, a jak szybko gdy go utworzymy
+-- aby sprawdzić jak szybko to zapytanie będzie działać na bazie jeśli indexu nie będzie,
+-- a jak szybko gdy go utworzymy
+
+CREATE INDEX idx_email ON "user" (email);
+
