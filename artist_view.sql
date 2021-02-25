1187c3f73d1e06c857f8ce908ba0f13679d48a1c
Magdalena Podolewska <magdalena@podolewska.com>
2021-02-25 13:12:56 +0100

    feat: view working but gives buck data in unexpected way TBC

--- artist_view.sql
+++ artist_view.sql
@@ -1,18 +1,18 @@
 --
 -- 2. Widok ze wszystkimi informacjami o piosenkach danego artysty
 
-
 CREATE OR REPLACE VIEW artist_view AS
     SELECT song.name, song.date, song.duration, artist.id FROM song
     LEFT JOIN song_artist ON song.id = song_artist.song_id
     LEFT JOIN artist ON artist.id = song_artist.artist_id;
 
 CREATE OR REPLACE FUNCTION get_artist_songs(id_num int)
-    RETURNS TABLE(i int)
+    RETURNS TABLE(name_var CHAR(150), data_var DATE, duration_var DOUBLE PRECISION)
     AS
     $$
     BEGIN
-        SELECT name, date, duration FROM artist_view WHERE id = $1;
+        RETURN QUERY
+        SELECT name, date, duration FROM artist_view WHERE id = id_num;
     END;
     $$ language plpgsql;
 
